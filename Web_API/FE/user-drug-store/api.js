c08394a866b206406cf93f640dbbfe65066566c3
vynhatduy <89023717+vynhatduy@users.noreply.github.com>
2024-03-25 17:55:19 +0700

    Update api.js

--- Web_API/FE/user-drug-store/api.js
+++ Web_API/FE/user-drug-store/api.js
@@ -1,4 +1,13 @@
 const api_url={
+    // dữ liệu trả về của sản phẩm bao gồm: id, ten, moTa, danhMucId, url, thanhPhan, dacDiem, congDung, chiDinh, lieuLuong, gia, nhaCungCap
     api_product:"https://localhost:7258/api/SanPhams",
-    api_categories:"https://localhost:7258/api/DanhMucs"
+    // dữ liệu trả về của danh mục bao gồm: id, ten, moTa
+    api_categories:"https://localhost:7258/api/DanhMucs",
+    // dữ liệu trả về của CTHoaDon bao gồm: hoaDonId, sanPhamId, soLuong, moTa, gia
+    api_ctHoaDon:"https://localhost:7258/api/CTHoaDons",
+    // dữ liệu trả về của HoaDon bao gồm: id, sdt, ngay, moTa, tongGia
+    api_hoaDon:"https://localhost:7258/api/HoaDons",
+    //dữ liệu trả về của NguoiDung bao gồm: ten, diaChi,sdt,mk,quyenId
+    api_nguoiDung:"https://localhost:7258/api/NguoiDungs"
+    
 }
